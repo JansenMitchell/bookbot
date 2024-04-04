7d035970f8b6e27b3b005f866a59883c86be80d1
JansenMitchell <jansenmitchellp@gmail.com>
2024-04-04 16:43:15 -0500

    Refactor main.py to use a separate function for reading book text

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
-    with open('books/frankenstein.txt', 'r') as f:
-        text = f.read()
-        print(text)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
     
 main()
