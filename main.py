ab56b2cffee99d45a5ccbe81286d538a40127064
JansenMitchell <jansenmitchellp@gmail.com>
2024-04-04 17:11:50 -0500

    Add letter count functionality to main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
-    print(f"Word count: {word_count}")
+    letter_count = get_letter_count(text)
+    print(f"Letter count: {letter_count}")
 
 
 def get_book_text(path):
@@ -11,5 +12,20 @@ def get_book_text(path):
     
 def get_word_count(text):
     return len(text.split())
+
+def get_letter_count(text):
+    # TODO: Split each word into letters and count them
+    # TODO: Make each letter lowercase
+    # TODO: Return the total count of letters in a dictionary
+    letter_dict = {}
+    
+    for letter in text:
+        if letter.isalpha():
+            letter = letter.lower()
+            if letter in letter_dict:
+                letter_dict[letter] += 1
+            else:
+                letter_dict[letter] = 1
+    return letter_dict
     
 main()
