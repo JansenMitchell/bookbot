35216024cedc841a85df1f6cb85f0d729da11287
JansenMitchell <jansenmitchellp@gmail.com>
2024-10-17 16:35:22 -0500

    Add book report output to main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    def sort_on(dict):
+        return dict["num"]
+    
+    char_dict = count_characters()
+    char_list = [{"char": char, "num": count} for char, count in char_dict.items()]
+    char_list.sort(reverse=True, key=sort_on)
+    
+    count = count_words()
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count} words found in the document\n")
+    for char_info in char_list:
+        char = char_info['char']
+        count = char_info['num']
+        if char.isalpha():  # Only print alphabetic characters
+            print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
         
 def count_words():
     with open("books/frankenstein.txt") as f:
@@ -25,7 +39,5 @@ def count_characters():
             for character in word_string:
                 character_dict[character] = character_dict.get(character, 0) + 1
         return character_dict
-            
-
+    
 main()
-count_characters()
