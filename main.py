f88c15c4f8bbea32773da51467a4226b968c8069
JansenMitchell <jansenmitchellp@gmail.com>
2025-12-10 14:02:30 -0600

    Update main.py with get_book_text feature

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file, "r") as file:
+        return file.read()
+
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+
+main()
