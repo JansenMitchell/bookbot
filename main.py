0a001ab4b1aaef948ff0dcee1134ed4da4414ca7
JansenMitchell <jansenmitchellp@gmail.com>
2025-12-11 13:40:23 -0600

    Add main guard and rename book path variable
    Update usage prompt to reference python3 and show <path_to_book> to
    match tests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 if len(sys.argv) != 2:
-    print("Usage: python main.py <book_path>")
+    print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
 from stats import count_characters, count_words, sort_dict_list
@@ -12,9 +12,9 @@ def get_book_text(path_to_file):
         return file.read()
 
 
-def print_report(book_path, word_count, sorted_characters):
+def print_report(path_to_book, word_count, sorted_characters):
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}")
+    print(f"Analyzing book found at {path_to_book}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
@@ -26,12 +26,13 @@ def print_report(book_path, word_count, sorted_characters):
 
 
 def main():
-    book_path = sys.argv[1]
-    book_text = get_book_text(book_path)
+    path_to_book = sys.argv[1]
+    book_text = get_book_text(path_to_book)
     word_count = count_words(book_text)
     character_count = count_characters(book_text)
     sorted_characters = sort_dict_list(character_count)
-    print_report(book_path, word_count, sorted_characters)
+    print_report(path_to_book, word_count, sorted_characters)
 
 
-main()
+if __name__ == "__main__":
+    main()
